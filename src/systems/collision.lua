f64aca59959371b190c11e0f187429dd1d24ff71
Cole Bambrick <kcirbmab@gmail.com>
2013-08-20 05:00:28 -0400

    removed unused functions from collision detection
    also includes minor refactoring

--- src/systems/collision.lua
+++ src/systems/collision.lua
@@ -1,8 +1,8 @@
 --[[----------------------------------------------------------------------------
 --]]----------------------------------------------------------------------------
 
-local resolveCollision, getHitboxPosition, getLogicalHitboxes, areColliding
-local flashRed
+local resolveCollision, updateHitboxCoordinates
+local flashRed, killEnemy
 
 ---------------------------------------------------------- MAIN UPDATE FUNCTION
 
@@ -14,7 +14,7 @@ secs.updatesystem("collision", 400, function(dt)
 	local space = secs.query("spatialMaps")[1].spatialhash.map
 	local collidableEntities = secs.query("collidable")
 	
-	-- update hitboxes
+	-- update spatial map and hitboxes
 	space:clear()
 	for i,e in ipairs(collidableEntities) do
 		for j,h in ipairs(e.hitboxes) do
@@ -27,21 +27,19 @@ secs.updatesystem("collision", 400, function(dt)
 	for i,a in ipairs(collidableEntities) do
 		for j,h1 in ipairs(a.hitboxes) do
 			for h2,b in pairs(space:getNearby(h1.x1, h1.y1, h1.x2, h1.y2)) do	
-				if a ~= b then
-					if h1.x1 < h2.x2 and h1.x2 > h2.x1 and h1.y1 < h2.y2 and h1.y2 > h2.y1 then
-						resolveCollision(a, b, h1.type, h2.type)
-					end
+				if a ~= b
+				and h1.x1 < h2.x2 and h1.x2 > h2.x1 
+				and h1.y1 < h2.y2 and h1.y2 > h2.y1 then
+					resolveCollision(a, b, h1.type, h2.type)
 				end
 			end
 		end
 	end
 	
 	-- update hitboxes
-	space:clear()
 	for i,e in ipairs(collidableEntities) do
 		for j,h in ipairs(e.hitboxes) do
 			updateHitboxCoordinates(e, h)
-			space:insert(h, e, h.x1, h.y1, h.x2, h.y2)
 		end
 	end
 	
@@ -49,48 +47,38 @@ end)
 
 -------------------------------------------------------------- HELPER FUNCTIONS
 
-function getLogicalHitboxes(e)
-	local hitboxes = {}
-	for i,hitbox in ipairs(e.hitboxes) do
-		if hitbox.type ~= "passive" then
-			local h = { type = hitbox.type }
-			h.x1, h.x2, h.y1, h.y2 = getHitboxPosition(e, hitbox)
-			table.insert(hitboxes, h)
-		end
-	end
-	return hitboxes
-end
-
-function areColliding(a, b, h1, h2)
-	local ax1,ax2,ay1,ay2 = getHitboxPosition(a, h1)
-	local bx1,bx2,by1,by2 = getHitboxPosition(b, h2)
-	if ax1 < bx2 and ax2 > bx1 and ay1 < by2 and ay2 > by1 then
-		return true
-	else
-		return false
-	end
-end
-
-function getHitboxPosition(e, hitbox)
-	local x1,x2,y1,y2
+function updateHitboxCoordinates(e, hitbox)
 	if e.pos.dx >= 0 then
-		x1 = e.pos.x + hitbox.offsetX
+		hitbox.x1 = e.pos.x + hitbox.offsetX
 	else
-		x1 = e.pos.x - hitbox.offsetX - hitbox.width  + hitbox.centerX * 2
+		hitbox.x1 = e.pos.x - hitbox.offsetX - hitbox.width + hitbox.centerX*2
 	end
 	if e.pos.dy >= 0 then
-		y1 = e.pos.y + hitbox.offsetY
+		hitbox.y1 = e.pos.y + hitbox.offsetY
 	else
-		y1 = e.pos.y - hitbox.offsetY - hitbox.height + hitbox.centerY * 2
+		hitbox.y1 = e.pos.y - hitbox.offsetY - hitbox.height + hitbox.centerY*2
 	end
-	x2 = x1 + hitbox.width
-	y2 = y1 + hitbox.height
-	
-	return x1,x2,y1,y2
+	hitbox.x2 = hitbox.x1 + hitbox.width
+	hitbox.y2 = hitbox.y1 + hitbox.height
 end
 
-
 --------------------------------------------------- RESOLUTION HELPER FUNCTIONS
+
+function flashRed(e)
+	local c = coroutine.create(function(e)
+		if not e.color then
+			secs.attach(e, "color", { rgb = { 255, 0, 0, 255 } })
+		end
+		for i = 1,3 do
+			e.color.rgb = { 255, 0, 0, 255 }
+			wait(0.1)
+			e.color.rgb = { 255, 255, 255, 0 }
+			wait(0.1)
+		end
+		e.color.rgb = { 255, 255, 255, 255 }
+	end)
+	coroutine.resume(c, e)
+end
 
 function killEnemy(e)
 	flashRed(e)
@@ -108,22 +96,6 @@ function killEnemy(e)
 			end, e)
 		end
 	end
-end
-
-function flashRed(e)
-	local c = coroutine.create(function(e)
-		if not e.color then
-			secs.attach(e, "color", { rgb = { 255, 0, 0, 255 } })
-		end
-		for i = 1,3 do
-			e.color.rgb = { 255, 0, 0, 255 }
-			wait(0.1)
-			e.color.rgb = { 255, 255, 255, 0 }
-			wait(0.1)
-		end
-		e.color.rgb = { 255, 255, 255, 255 }
-	end)
-	coroutine.resume(c, e)
 end
 
 ---------------------------------------------------------- COLLISION RESOLUTION
