3198d801b23dc7e7f6da93bcc61e4bdc4b239929
Cole Bambrick <kcirbmab@gmail.com>
2014-08-09 19:00:18 -0400

    Added utility.math.getCenter() function
    Returns the center point of the given object. Also performed minor
    refactoring on other utility functions.

--- src/lib/utility.lua
+++ src/lib/utility.lua
@@ -1,32 +1,48 @@
-local Utility = { math = {}, table = {} }
+local utility = { math = {}, table = {} }
 
 --------------------------------------------------------------------------------
--- Returns true if the first paramet is located within the boundaries of the
--- second parameter.
--- @param a		The first parameter, which has x and y members.
--- @param b		The seond parameter, which has x, y, width, and height members.
+-- Returns true if the first object is located within the boundaries of the
+-- second object.
+-- @param a     The first object, which has x and y members.
+-- @param b     The seond object, which has x, y, width, and height members.
+-- @return      True if the first object intersects with the second object;
+--              otherwise, false.
 --------------------------------------------------------------------------------
-function Utility.math.within(a, b)
-	return a.x <  b.x + b.width
-	   and a.x >= b.x
-	   and a.y >= b.y
-	   and a.y <  b.y + b.height	
+function utility.math.within(a, b)
+    return a.x <  b.x + b.width
+       and a.x >= b.x
+       and a.y >= b.y
+       and a.y <  b.y + b.height    
+end
+
+--------------------------------------------------------------------------------
+-- Returns the center point of the given object.
+-- @param object    The object, which has x, y, width, and height members.
+-- @return          The x and y coordinates of the center of the object.
+--------------------------------------------------------------------------------
+function utility.math.getCenter(object)
+    return {
+        x = object.x + object.width / 2,
+        y = object.y + object.height / 2,
+    }
 end
 
 --------------------------------------------------------------------------------
 -- Returns the value of the table element that satisfies the given expression.
 -- The expression function must return true or false.
--- @param t				The table.
--- @param expression	The expression function to check against.
+-- @param t             The table.
+-- @param expression    The expression function to check against
+-- @return              The first element that satifies the expression, or nil 
+--                      if no element is found.
 --------------------------------------------------------------------------------
-function Utility.table.find(t, expression)
-	for i,v in pairs(t) do
-		if expression(t[i]) then
-			return v
-		end
-	end
-	
-	return nil
+function utility.table.find(t, expression)
+    for i, v in pairs(t) do
+        if expression(t[i]) then
+            return v
+        end
+    end
+    
+    return nil
 end
 
-return Utility
+return utility
