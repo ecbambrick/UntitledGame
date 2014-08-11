d38349776fc482a720a2eab2ec7d2a19222a6390
Cole Bambrick <kcirbmab@gmail.com>
2014-08-11 09:57:24 -0400

    Refactored Timer class
    Added comments and modified implementation.

--- src/lib/timer.lua
+++ src/lib/timer.lua
@@ -1,24 +1,43 @@
+local Timer = {}
+
+--------------------------------------------------------------------------------
+-- Returns the amount of time elapsed since the last time the timer was reset.
+-- @return The amount of time elapsed.
+--------------------------------------------------------------------------------
+function Timer:elapsed()
+    return self._count
+end
+
+--------------------------------------------------------------------------------
+-- Resets the timer's elapsed time to the given amount, or 0 if no given amount.
+-- @param count The amount of time to set the count to.
+--------------------------------------------------------------------------------
+function Timer:reset(count)
+    self._count = count or 0
+end
+
+--------------------------------------------------------------------------------
+-- Updates the timer's count.
+-- @param dt    The amount of time passed since the last update.
+-- @return      True if the threshold has been exceeded; otherwise, false.
+--------------------------------------------------------------------------------
+function Timer:update(dt)
+    self._count = self._count + dt
+    return self._count > self._threshold
+end
+
+--------------------------------------------------------------------------------
+-- Constructs a new instance of the timer.
+-- @return a new instance of the timer.
+--------------------------------------------------------------------------------
 return function(threshold)
-	return {
-		count = 0,
-		threshold = threshold,
-		
-		update = function(self, dt)
-			self.count = self.count + dt
-			if self.count > self.threshold then
-				self.count = 0
-				return true
-			else
-				return false
-			end
-		end,
-		
-		ready = function(self)
-			return self.count == 0
-		end,
-		
-		reset = function(self)
-			self.count = 0
-		end,
-	}
+    local self = {}
+    
+    -- The amount of time elapsed since the last reset.
+    self._count = 0
+    
+    -- The amount of time the timer counts up to to activate.
+    self._threshold = threshold
+    
+    return setmetatable(self, { __index = Timer })
 end
